bbf8eced260e7180b7c6e4d58a9f1ed137bc56ae
Pham Minh Dung <224001528+mdpham412@users.noreply.github.com>
2025-08-25 22:08:14 +0700

    1.7

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -16,21 +16,45 @@ const Display = (props) => {
   )
 }
 
+const DisplayPercentage = (props) => {
+  return (
+    <>
+      <p>{props.name} {props.value * 100}%</p>
+    </>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAvg] = useState(0)
 
   const incrementGood = () => {
     setGood(good+1)
+    incrementAll()
+    incrementTotal()
   }
   const incrementNeutral = () => {
     setNeutral(neutral+1)
+    incrementAll()
   }
   const incrementBad = () => {
     setBad(bad+1)
+    incrementAll()
+    decrementTotal()
+  }
+  const incrementAll = () => {
+    setAll(all+1)
+  }
+  const incrementTotal = () => {
+    setTotal(total+1)
+  }
+  const decrementTotal = () => {
+    setTotal(total-1)
   }
 
 
@@ -45,6 +69,9 @@ const App = () => {
       <Display name="good" value={good}/>
       <Display name="neutral" value={neutral}/>
       <Display name="bad" value={bad}/>
+      <Display name="all" value={all}/>
+      <Display name="average" value={total/all}/>
+      <DisplayPercentage name="positive" value={good/all}/>
     </div>
   )
 }

123faae3ad7134e2269b8c51c14e64a6b5d3f15c
Pham Minh Dung <224001528+mdpham412@users.noreply.github.com>
2025-09-07 21:29:51 +0700

    2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const addName = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat(
+      {
+        name: newName,
+      }
+    ))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      {/* <div>debug: {newName}</div> */}
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input 
+          value={newName}
+          onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button 
+          type="submit"
+          onClick={addName}
+          >
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((entry) => (
+          <div>
+            {entry.name}
+          </div>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App

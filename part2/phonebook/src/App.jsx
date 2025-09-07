3e26c78c3f309d398678b9f1460bc17b45ceeea8
Pham Minh Dung <224001528+mdpham412@users.noreply.github.com>
2025-09-07 21:53:55 +0700

    2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,13 +2,23 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
-  const addName = (event) => {
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const addEntry = (event) => {
     event.preventDefault()
 
     const found = persons.find(
@@ -23,39 +33,43 @@ const App = () => {
     setPersons(persons.concat(
       {
         name: newName,
+        number: newNumber,
+        id: persons.length > 0 ? persons[persons.length - 1].id + 1 : 1
       }
     ))
+
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       {/* <div>debug: {newName}</div> */}
       <h2>Phonebook</h2>
+      <h2>Add entry</h2>
       <form>
         <div>
-          name: <input 
-          value={newName}
-          onChange={handleNameChange}
-          />
+          name: 
+          <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
-          <button 
-          type="submit"
-          onClick={addName}
-          >
+          number: 
+          <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addEntry}>
             add
           </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
+      <div>
         {persons.map((entry) => (
-          <div>
-            {entry.name}
+          <div key={entry.id}>
+            {entry.name}: {entry.number}
           </div>
         ))}
-      </ul>
+      </div>
     </div>
   )
 }

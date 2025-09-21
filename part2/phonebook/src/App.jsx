0a5104248bde54ebe82d42717cf8d8cebd843754
Pham Minh Dung <224001528+mdpham412@users.noreply.github.com>
2025-09-21 15:08:40 +0700

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 // import Filter from './components/Filter'
 
 const Filter = ({searchTerm, setSearchTerm}) => {
@@ -52,16 +53,18 @@ const PersonForm = ({newName, handleNameChange, newNumber, handleNumberChange, a
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
+  useEffect((() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then((response) => {
+      console.log('request done')
+      setPersons(response.data)
+    })
+  }), [])
   const handleNameChange = (event) => 
     setNewName(event.target.value)
   const handleNumberChange = (event) => 
